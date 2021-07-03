2cce8b388841f7d3af5885aa1da4dbf017c45b3c
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2021-07-03 23:25:43 +0900

    検索フォームのinputの値をonChangeイベントで取得してinput内に表示させる

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import axios from 'axios';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [result, setResult] = useState('');
+  const [keyword, setkeyword] = useState('')
+  const handleChange = (e) => {
+    const newKeyword = e.target.value;
+    setkeyword(newKeyword);
+  }
+
   return (
     <div className="App">
       <h1 className="title is-1">Book Seach</h1>
       <div className="seach_area">
         <div className="block">
           <div className="control has-icons-right">
-              <input className="input" type="text" placeholder="キーワード" />
+            <input
+              className="input"
+              type="text"
+              placeholder="キーワード"
+              value={keyword}
+              onChange={handleChange}
+            />
               <span className="icon is-small is-right">
                 <i className="fas fa-search"></i>
               </span>
@@ -15,15 +30,16 @@ function App() {
         </div>
         <div className="block">
           <div className="buttons">
-          <button class="button is-primary">検索</button>
+          <button className="button is-primary">検索</button>
         </div>
       
         </div>
       </div>
-      <div className="result_area">
+      {result
+        && <div className="result_area">
         <h3 className="title is-4">検索結果</h3>
         <div className="cards">
-          <div class="card block">
+          <div className="card block">
             <div className="card-image">
               <figure className="image is-2by3">
                 <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
@@ -43,7 +59,7 @@ function App() {
               </div>
             </div>
           </div>
-           <div class="card block">
+           <div className="card block">
             <div className="card-image">
               <figure className="image is-2by3">
                 <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" width="280px" />
@@ -95,7 +111,8 @@ function App() {
 
         </div>
       
-      </div>
+      </div>}
+
     </div>
   );
 }
